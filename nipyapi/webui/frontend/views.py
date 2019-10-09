08447677298c0c71c888321c85e2e1bb796a5f33
Andrew I. Christianson <andy@andyic.org>
2019-10-09 11:31:26 -0400

    initial bg tasks

--- nipyapi/webui/frontend/views.py
+++ nipyapi/webui/frontend/views.py
@@ -1,5 +1,17 @@
+from background_task import background
 from django.shortcuts import render
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@background(schedule=5)
+def notify_user(user_id):
+    logger.error('hello from background task')
+    print("HELLO")
 
 
 def index(request):
+    logger.error('hello from index view')
+    notify_user(10)
     return render(request, 'frontend/index.html')
--- nipyapi/webui/webui/settings.py
+++ nipyapi/webui/webui/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'kk^g_)&lyre$+t6w-4#w7c4+6$n%pvlgi2ht5nl0eft_*4@wt6'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,6 +38,7 @@ INSTALLED_APPS = [
     'nifi',
     'rest_framework',
     'frontend',
+    'background_task',
 ]
 
 MIDDLEWARE = [
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webui.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -115,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
